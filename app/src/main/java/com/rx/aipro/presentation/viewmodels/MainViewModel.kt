fc1c0530106a57f6b8ac4fe5aabfce111ff530aa
RxNaison <87330825+RxNaison@users.noreply.github.com>
2025-07-20 19:22:11 +0700

    Gemini list update

--- app/src/main/java/com/rx/aipro/presentation/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/rx/aipro/presentation/viewmodels/MainViewModel.kt
@@ -16,10 +16,11 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 val availableGeminiModels = listOf(
-    "gemini-2.5-flash-preview-04-17",
+    "gemini-2.5-flash",
     "gemini-2.0-flash",
-    "gemini-2.5-pro-preview-05-06",
-    "gemini-2.5-pro"
+    "gemini-2.5-pro",
+    "gemini-2.0-flash-preview-image-generation",
+    "gemini-2.0-flash-lite"
 )
 
 data class MainUiState(
